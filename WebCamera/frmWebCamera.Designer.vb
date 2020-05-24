1ade85b0bb482bf20ccc37a902d737dacd233504
ProgrammerAdityaRaj <ar19971@outlook.com>
2020-05-24 17:36:24 +0530

    first commit

---
+++ WebCamera/frmWebCamera.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWebCamera
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnCapture = New System.Windows.Forms.Button()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.TableLayoutPanel1.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox2, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.PictureBox1, 0, 0)
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(12, 12)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 1
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(722, 356)
+        Me.TableLayoutPanel1.TabIndex = 0
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(364, 3)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(355, 350)
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(355, 350)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(15, 407)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 1
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnCapture
+        '
+        Me.btnCapture.Location = New System.Drawing.Point(334, 407)
+        Me.btnCapture.Name = "btnCapture"
+        Me.btnCapture.Size = New System.Drawing.Size(75, 23)
+        Me.btnCapture.TabIndex = 2
+        Me.btnCapture.Text = "Capture"
+        Me.btnCapture.UseVisualStyleBackColor = True
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(656, 407)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 3
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'frmWebCamera
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Blue
+        Me.ClientSize = New System.Drawing.Size(746, 442)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnCapture)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Name = "frmWebCamera"
+        Me.Text = "Web Camera"
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnCapture As Button
+    Friend WithEvents btnSave As Button
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+End Class
---
+++ WebCamera/frmWebCamera.vb
@@ -0,0 +1,48 @@
+﻿Imports AForge
+Imports AForge.Video
+Imports AForge.Video.DirectShow
+Imports System.IO
+Imports System.Windows
+
+Public Class frmWebCamera
+    Dim camera As VideoCaptureDevice
+
+    Dim bmp As Bitmap
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        Dim cameras As VideoCaptureDeviceForm = New VideoCaptureDeviceForm()
+
+        If cameras.ShowDialog() = System.Windows.Forms.DialogResult.OK Then
+            camera = cameras.VideoDevice
+            AddHandler camera.NewFrame, New NewFrameEventHandler(AddressOf captured)
+            camera.Start()
+        End If
+    End Sub
+    Private Sub captured(Sender As Object, eventargs As NewFrameEventArgs)
+        bmp = DirectCast(eventargs.Frame.Clone(), Bitmap)
+        PictureBox1.Image = DirectCast(eventargs.Frame.Clone(), Bitmap)
+
+    End Sub
+
+    Private Sub btnCapture_Click(sender As Object, e As EventArgs) Handles btnCapture.Click
+        PictureBox2.Image = PictureBox1.Image
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        SaveFileDialog1.DefaultExt = ".jpg"
+        If SaveFileDialog1.ShowDialog = System.Windows.Forms.DialogResult.OK Then
+            PictureBox2.Image.Save(SaveFileDialog1.FileName, Imaging.ImageFormat.Jpeg)
+        End If
+
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        camera.Stop()
+    End Sub
+End Class
+
